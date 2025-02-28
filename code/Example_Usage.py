c4a5b317f34e6e39cdd50d0ddd845826bcb5810e
Rikka <t_rikka@icloud.com>
2025-02-27 22:06:59 -0500

    Update Example_Usage.py

--- code/Example_Usage.py
+++ code/Example_Usage.py
@@ -12,7 +12,9 @@ def send_key(modifier, key):
 KEY_A = 0x04
 KEY_ENTER = 0x28
 MOD_LEFT_CTRL = 0x01
+MOD_LEFT_ALT = 0x04
 
 send_key(0, KEY_A) # a
 send_key(MOD_LEFT_CTRL, KEY_A) # ctrl a
+send_key(MOD_LEFT_CTRL | MOD_LEFT_ALT, KEY_A) # ctrl alt a
 send_key(0, KEY_ENTER) # enter
