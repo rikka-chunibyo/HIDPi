2ec604f485275964fd3ba1769c179080bf444534
Rikka <t_rikka@icloud.com>
2025-02-28 17:57:55 -0500

    Create setup.py

---
+++ code/library/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+from my_pip_package import __version__
+
+setup(
+    name='hidpi',
+    version=__version__,
+
+    url='https://github.com/rikka-chunibyo/HIDPi',
+    author='Rikka Takanashi',
+    author_email='',
+
+    py_modules=['hidpi'],
+)
