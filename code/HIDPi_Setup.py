4092936952dbceeb37bc36d23b12ec17474f6f71
Rikka <t_rikka@icloud.com>
2025-03-02 17:20:42 -0500

    ADDED MOUSE

--- code/HIDPi_Setup.py
+++ code/HIDPi_Setup.py
@@ -74,33 +74,54 @@ def setup_hid_gadget():
         "mkdir -p /sys/kernel/config/usb_gadget/hid_gadget/strings/0x409",
         "echo '1234567890' > /sys/kernel/config/usb_gadget/hid_gadget/strings/0x409/serialnumber",
         "echo 'Rikka' > /sys/kernel/config/usb_gadget/hid_gadget/strings/0x409/manufacturer",
-        "echo 'HIDPi' > /sys/kernel/config/usb_gadget/hid_gadget/strings/0x409/product",
-        "mkdir -p /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0",
-        "echo 1 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0/protocol",
-        "echo 1 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0/subclass",
-        "echo 8 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0/report_length"
+        "echo 'HIDPi' > /sys/kernel/config/usb_gadget/hid_gadget/strings/0x409/product"
     ]
-    
+
     for cmd in commands:
         run_command(cmd)
 
-    report_desc = b"\x05\x01\x09\x06\xa1\x01\x05\x07\x19\xe0\x29\xe7\x15\x00\x25\x01\x75\x01\x95\x08\x81\x02\x95\x01\x75\x08\x81\x01\x95\x05\x75\x01\x05\x08\x19\x01\x29\x05\x91\x02\x95\x01\x75\x03\x91\x01\x95\x06\x75\x08\x15\x00\x26\xa4\x00\x05\x07\x19\x00\x29\xa4\x81\x00\xc0"
-    
-    with open("/tmp/report_desc.bin", "wb") as f:
-        f.write(report_desc)
-    
-    run_command("sudo cat /tmp/report_desc.bin > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0/report_desc")
-    
-    commands = [
-        "mkdir -p /sys/kernel/config/usb_gadget/hid_gadget/configs/c.1/strings/0x409",
-        "echo 'Config 1' > /sys/kernel/config/usb_gadget/hid_gadget/configs/c.1/strings/0x409/configuration",
-        "echo 250 > /sys/kernel/config/usb_gadget/hid_gadget/configs/c.1/MaxPower",
-        "ln -s /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0 /sys/kernel/config/usb_gadget/hid_gadget/configs/c.1/",
-        "ls /sys/class/udc > /sys/kernel/config/usb_gadget/hid_gadget/UDC"
-    ]
-    
-    for cmd in commands:
-        run_command(cmd)
+    # KEYBOARD
+    run_command("mkdir -p /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0")
+    run_command("echo 1 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0/protocol")
+    run_command("echo 1 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0/subclass")
+    run_command("echo 8 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0/report_length")
+
+    keyboard_desc = b"\x05\x01\x09\x06\xa1\x01\x05\x07\x19\xe0\x29\xe7\x15\x00\x25\x01\x75\x01\x95\x08\x81\x02\x95\x01\x75\x08\x81\x01\x95\x05\x75\x01\x05\x08\x19\x01\x29\x05\x91\x02\x95\x01\x75\x03\x91\x01\x95\x06\x75\x08\x15\x00\x26\xa4\x00\x05\x07\x19\x00\x29\xa4\x81\x00\xc0"
+    with open("/sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0/report_desc", "wb") as f:
+        f.write(keyboard_desc)
+
+    # MOUSE
+    run_command("mkdir -p /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb1")
+    run_command("echo 1 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb1/protocol")
+    run_command("echo 1 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb1/subclass")
+    run_command("echo 4 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb1/report_length")
+
+    mouse_desc = b"\x05\x01\x09\x02\xa1\x01\x09\x01\xa1\x00\x05\x09\x19\x01\x29\x03\x15\x00\x25\x01\x75\x01\x95\x03\x81\x02\x75\x05\x95\x01\x81\x01\x05\x01\x09\x30\x09\x31\x15\x81\x25\x7f\x75\x08\x95\x02\x81\x06\xc0\xc0"
+    with open("/sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb1/report_desc", "wb") as f:
+        f.write(mouse_desc)
+
+    # CONTROLLER
+    # run_command("mkdir -p /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb2")
+    # run_command("echo 1 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb2/protocol")  # Boot protocol
+    # run_command("echo 1 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb2/subclass")  # Gamepad subclass
+    # run_command("echo 64 > /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb2/report_length")
+
+    # gamepad_desc = b"\x05\x01\x09\x05\xa1\x01\xa1\x02\x85\x01\x05\x09\x19\x01\x29\x10\x15\x00\x25\x01\x75\x01\x95\x10\x81\x02\x05\x01\x09\x30\x09\x31\x09\x32\x09\x35\x15\x81\x25\x7f\x75\x08\x95\x04\x81\x02\xc0\xc0"
+    # with open("/sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb2/report_desc", "wb") as f:
+    #     f.write(gamepad_desc)
+
+
+    # CONFiGURE GADGET
+    run_command("mkdir -p /sys/kernel/config/usb_gadget/hid_gadget/configs/c.1/strings/0x409")
+    run_command("echo 'Config 1' > /sys/kernel/config/usb_gadget/hid_gadget/configs/c.1/strings/0x409/configuration")
+    run_command("echo 250 > /sys/kernel/config/usb_gadget/hid_gadget/configs/c.1/MaxPower")
+
+    # LiNK FUNCTiONS TO GADGET CONFiG
+    run_command("ln -s /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb0 /sys/kernel/config/usb_gadget/hid_gadget/configs/c.1/")
+    run_command("ln -s /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb1 /sys/kernel/config/usb_gadget/hid_gadget/configs/c.1/")
+    # run_command("ln -s /sys/kernel/config/usb_gadget/hid_gadget/functions/hid.usb2 /sys/kernel/config/usb_gadget/hid_gadget/configs/c.1/")
+
+    run_command("ls /sys/class/udc > /sys/kernel/config/usb_gadget/hid_gadget/UDC")
 
 def create_udev_rule():
     print("Creating udev rule for hidg...")
@@ -109,7 +130,7 @@ def create_udev_rule():
         f.write('KERNEL=="hidg*", MODE="0666"\n')
     
     run_command("sudo udevadm control --reload-rules")
-    run_command("sudo chmod 666 /dev/hidg0")
+    run_command("sudo chmod 666 /dev/hidg*")
     print("Udev rules reloaded.")
 
 def main():
