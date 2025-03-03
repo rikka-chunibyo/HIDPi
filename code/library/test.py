50947d0b851f57d280be264803ed9c38d9aea414
Rikka <t_rikka@icloud.com>
2025-03-02 20:32:22 -0500

    Update test.py

--- code/library/test.py
+++ code/library/test.py
@@ -1,5 +1,7 @@
 import time
-from hidpi.* import *
+import math
+
+from hidpi import Keyboard, Mouse
 from hidpi.keyboard_keys import *
 from hidpi.mouse_buttons import *
 
@@ -8,20 +10,22 @@ def test_keyboard():
     time.sleep(1)
     Keyboard.send_text("Hello, HIDPi!")
     time.sleep(1)
-    Keyboard.hold_key(0x00, KEY_ENTER)
-    time.sleep(3)
+    Keyboard.hold_key(0, KEY_A)
+    time.sleep(1)
     Keyboard.release_keys()
     time.sleep(1)
 
 def test_mouse():
     print("Testing mouse...")
     time.sleep(1)
-    Mouse.move(50, 50)
-    time.sleep(1)
-    Mouse.click(LEFT)
-    time.sleep(1)
-    Mouse.move(-50, -50)
-    time.sleep(1)
+    
+    radius = 5
+    
+    for angle in range(0, 360, 1):
+        x = int(radius * math.cos(math.radians(angle)))
+        y = int(radius * math.sin(math.radians(angle)))
+        Mouse.move(x, y)
+        time.sleep(0.01)
 
 if __name__ == "__main__":
     test_keyboard()
